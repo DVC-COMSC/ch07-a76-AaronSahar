3dfff650951160719786232759e3ca3cbf646771
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 23:02:21 -0700

    test

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
-
-# ******************************
-# Make your Code
-# ******************************
-
-# print (num3) 
+num1 = list(map(int, input("Enter the numbers ").split()))
+num2 = list(map(int, input("Enter the numbers ").split()))
+if len(num1) <= len(num2):
+    merged = num1 + num2
+else:
+    merged = num2 + num1
+print(merged)
